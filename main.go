4247e681f101bf60495852aeb4c43db2e90e546e
Eduardo Narimatsu <eduardo.ekn@gmail.com>
2023-10-14 17:33:48 -0300

    Remove comments

--- main.go
+++ main.go
@@ -3,17 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	//var card string = "Ace of Spades"
-	//card := newCard()
-	//fmt.Println(newCard())
-
-	// cards := newDeck()
-
-	// hand, remainingCards := deal(cards,5)
-
-	// hand.print()
-	// remainingCards.print()
-
 	cards := newDeck()
 	fmt.Println(cards.toString())
 }
