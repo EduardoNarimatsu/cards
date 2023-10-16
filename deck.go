713525b569e1daffb294a79f75c0d328884270b1
Eduardo Narimatsu <eduardo.ekn@gmail.com>
2023-10-16 14:49:40 -0300

    Alter function

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -10,7 +11,7 @@ import (
 
 type deck []string
 
-func newDeck() deck{
+func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
@@ -18,7 +19,7 @@ func newDeck() deck{
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, value + " of " + suit)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 	return cards
@@ -26,14 +27,18 @@ func newDeck() deck{
 
 func (d deck) print() {
 	for i, card := range d {
-		fmt.Println(i,card)
+		fmt.Println(i, card)
 	}
-} 
+}
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
 func (d deck) toString() string {
-	return strings.Join([]string(d), ",")	
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
